3bc1990cc7ad8a455589e83ef46366d0db0c1842
Andrei Dzebysh <presnia@yahoo.com>
2022-08-26 21:59:41 +0300

    Slightly corrected

--- src/app/components/Pagination.jsx
+++ src/app/components/Pagination.jsx
@@ -9,7 +9,7 @@ const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
     return (
         <nav aria-label="Page navigation example">
             <ul className="pagination">
-                <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                <li className="page-item"><button className="page-link">Previous</button></li>
                 {
                     pages.map(page =>
                         <li
@@ -25,7 +25,7 @@ const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
                         </li>
                     )
                 }
-                <li className="page-item"><a className="page-link" href="#">Next</a></li>
+                <li className="page-item"><button className="page-link">Next</button></li>
             </ul>
         </nav>
     );
